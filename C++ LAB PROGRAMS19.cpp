b1fad9fd8f21eaa754bc04e744f61e19a037e7f1
Hari Krishna <94832444+hari890@users.noreply.github.com>
2022-03-21 09:24:48 +0530

    Create C++ LAB PROGRAMS19.cpp

---
+++ C++ LAB PROGRAMS19.cpp
@@ -0,0 +1,28 @@
+19./*Define class model for complex number, student class, book class and show it using UML 
+diagram as well as concrete class.*/ 
+#include<bits/stdc++.h> 
+using namespace std; 
+class BOOK 
+{ 
+public: 
+ string name,author; 
+ int price; 
+ 
+ void setName(); 
+ void setAuthor(); 
+ void setPrice(); 
+ void getName(); 
+ void getAuthor(); 
+ void getPrice(); 
+}; 
+class Student() 
+{ 
+public: 
+ string name,address; 
+ 
+ void setName(); 
+ void setAddress(); 
+ void getName(); 
+ void getAddress(); 
+}
+UML Diagram
